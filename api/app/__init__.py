bcd03903faae2c0ebdd8b40bdd2ea78414a0501e
ITRose225 <itrose225@gmail.com>
2022-04-02 17:04:17 -0500

    working remove bias endpoint

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from json import loads
-from app import summarize_text
+from app import summarize_text, remove_text_bias
 
 app = FastAPI()
 
@@ -10,6 +10,8 @@ async def ping():
 
 @app.post("/summarize-bias")
 async def summarize_bias(text: str = Body(...)):
-    #print(text)
-    #return text
     return summarize_text.summarize_text(text)
+
+@app.post("/remove-bias")
+async def remove_bias(text: str = Body(...)):
+    return remove_text_bias.remove_bias(text)
---
+++ api/app/remove_text_bias.py
@@ -0,0 +1,16 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAIAPIKEY")
+
+def remove_bias(text):
+
+    response = openai.Edit.create(
+    engine="text-davinci-edit-001",
+    input=text,
+    instruction="Remove sources of bias",
+    temperature=0.7,
+    top_p=1
+    )
+    
+    return response
