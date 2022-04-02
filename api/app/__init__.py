2170b79c86d2331b560fc95ec24c88dd0406efd8
ITRose225 <itrose225@gmail.com>
2022-04-02 16:43:11 -0500

    working api

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from json import loads
+from app import summarize_text
 
 app = FastAPI()
 
 @app.get("/")
 async def ping():
     return "pong"
+
+@app.post("/summarize-bias")
+async def summarize_bias(text: str = Body(...)):
+    #print(text)
+    #return text
+    return summarize_text.summarize_text(text)
