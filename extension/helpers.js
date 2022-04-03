f367cf36564678d6cb75ba0ef7de9f5141a8a60b
alexrpreston <apreston@smu.edu>
2022-04-03 03:14:36 -0500

    Styled Chrome Extension

--- extension/helpers.js
+++ extension/helpers.js
@@ -6,18 +6,19 @@ var getHTMLCODE = setInterval(function() {
     if ("Injecting Script...." !== htmlCode) {
     
         //Get Bias Level
-        // var url = "http://localhost:80/api/v1/classify-bias-level?type=html";
-        // fetch(url, {
-        //     method : "POST",
-        //     body: htmlCode
-        // })
-        // .then(response => response.text())
-        // .then(data => {
-        //     console.log("Data16",data)
-        //     document.getElementById("biaslevel").innerHTML = data;
-        //     // document.getElementById("biasRange").value = biasOptions.indexOf(data);
-        // });
+        var url = "http://localhost:80/api/v1/classify-bias-level?type=html";
+        fetch(url, {
+            method : "POST",
+            body: htmlCode
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log("Data16",data)
+            document.getElementById("biaslevel").innerHTML = data.replaceAll('"', '');
+            // document.getElementById("biasRange").value = biasOptions.indexOf(data);
+        });
         console.log("Line 20")
+
         //Get Reason for Bias
         var url = "http://localhost:80/api/v1/summarize-bias?type=html";
         fetch(url, {
@@ -27,8 +28,11 @@ var getHTMLCODE = setInterval(function() {
         .then(response => response.text())
         .then(data => { 
             console.log("Data29",data)
-            document.getElementById("reasonForBiasDiv").classList.remove("visually-hidden");
-            document.getElementById("biasReason").innerHTML = data;
+            let biasLevel = document.getElementById("biaslevel").innerHTML;
+            if(biasLevel !== "Not biased" || biasLevel !== "Couldn't classify bias level"){
+                document.getElementById("reasonForBiasDiv").classList.remove("visually-hidden");
+                document.getElementById("biasReason").innerHTML = data.replaceAll('"', '');
+            }
             // document.getElementById("biasRange").value = biasOptions.indexOf(data);
         })
         .catch(error => {
