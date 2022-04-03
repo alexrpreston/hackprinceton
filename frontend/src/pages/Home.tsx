267a90d185c2db79efc29cc50657a8012869b2a7
jlaneve <lanevejulian@gmail.com>
2022-04-02 19:10:36 -0500

    Add react icons and button

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
-import { Box, Container, Text, Center } from '@chakra-ui/react';
+import { Box, Container, Text, Center, HStack, Button, Spacer } from '@chakra-ui/react';
+
+import { FiRightArrow } from 'react-icons/fi';
 
 import Typist from '../components/Typist';
 
 const Home = () => (
   <Container>
     <Center width="100%" height="300px">
-      <Box as="h1" fontSize="6xl" fontWeight="bold">
-        <Typist text={'Right with Sway.'} />
+      <Box>
+        <Box as="h1" fontSize="6xl" fontWeight="bold">
+          <Typist text={'Right with Sway.'} />
+        </Box>
+
+        <Box>
+          <HStack>
+            <Spacer />
+            <Button>
+              Check out the Editor <FiRightArrow />
+            </Button>
+          </HStack>
+        </Box>
       </Box>
     </Center>
-    <Box height="1000px" bgColor="red">
+    <Box height="1000px" bgColor="gray">
       xyz
     </Box>
   </Container>
